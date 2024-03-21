e2752b1a8076b9fff654ac67263cdcde25d4137e
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-03-22 04:15:38 +0600

    Updated: updated the base query to logout for 401 response

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,19 +1,32 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { userLoggedOut } from '../auth/authSlice';
+
+const baseQuery = fetchBaseQuery({
+	baseUrl: 'http://localhost:9000',
+	prepareHeaders: (headers, { getState }) => {
+		const { accessToken } = getState().auth;
+
+		if (accessToken) {
+			headers.set('Authorization', 'Bearer ' + accessToken);
+		}
+
+		return headers;
+	},
+});
 
 const apiSlice = createApi({
 	reducerPath: 'api',
-	baseQuery: fetchBaseQuery({
-		baseUrl: 'http://localhost:9000',
-		prepareHeaders: (headers, { getState }) => {
-			const { accessToken } = getState().auth;
+	baseQuery: async (args, api, extraOptions) => {
+		const result = await baseQuery(args, api, extraOptions);
 
-			if (accessToken) {
-				headers.set('Authorization', 'Bearer ' + accessToken);
-			}
+		if (result?.error && result?.error?.status === 401) {
+			localStorage.removeItem('auth');
 
-			return headers;
-		},
-	}),
+			api.dispatch(userLoggedOut());
+		}
+
+		return result;
+	},
 	// eslint-disable-next-line no-unused-vars
 	endpoints: (builder) => ({}),
 });
