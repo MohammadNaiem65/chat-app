e9f20cbab49dce5bb01319d65604a8bf5b45be5e
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-03-22 03:11:11 +0600

    Updated: added customized headers inside baseQuery

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -4,6 +4,15 @@ const apiSlice = createApi({
 	reducerPath: 'api',
 	baseQuery: fetchBaseQuery({
 		baseUrl: 'http://localhost:9000',
+		prepareHeaders: (headers, { getState }) => {
+			const { accessToken } = getState().auth;
+
+			if (accessToken) {
+				headers.set('Authorization', 'Bearer ' + accessToken);
+			}
+
+			return headers;
+		},
 	}),
 	// eslint-disable-next-line no-unused-vars
 	endpoints: (builder) => ({}),
