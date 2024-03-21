1273c22347c6c4dfffcc1ecdae05bfedde2747c3
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-03-22 03:55:34 +0600

    Updated: made the ChatItems component from Sidebar dynamic

--- src/components/ChatItem.jsx
+++ src/components/ChatItem.jsx
@@ -1,26 +1,34 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import gravatarUrl from 'gravatar-url';
+import moment from 'moment';
 
-export default function ChatItem({ avatar, name, lastMessage, lastTime }) {
+export default function ChatItem({ conversation }) {
+	const { users, message, timestamp, id } = conversation;
+
+	const { user } = useSelector((state) => state.auth);
+	const participant = users.find((u) => u.id !== user.id);
+
 	return (
 		<Link
 			className='flex items-center px-3 py-2 text-sm transition duration-150 ease-in-out border-b border-gray-300 cursor-pointer hover:bg-gray-100 focus:outline-none'
-			to='/'>
+			to={`/inbox/${id}`}>
 			<img
 				className='object-cover w-10 h-10 rounded-full'
-				src={avatar}
-				alt={name}
+				src={gravatarUrl(participant.email)}
+				alt={participant.name}
 			/>
 			<div className='w-full pb-2 hidden md:block'>
 				<div className='flex justify-between'>
 					<span className='block ml-2 font-semibold text-gray-600'>
-						{name}
+						{participant.name}
 					</span>
 					<span className='block ml-2 text-sm text-gray-600'>
-						{lastTime}
+						{moment(timestamp).fromNow()}
 					</span>
 				</div>
 				<span className='block ml-2 text-sm text-gray-600'>
-					{lastMessage}
+					{message}
 				</span>
 			</div>
 		</Link>
--- src/components/ChatItems.jsx
+++ src/components/ChatItems.jsx
@@ -1,27 +1,54 @@
+import { useSelector } from 'react-redux';
+import { useGetConversationsQuery } from '../features/conversation/conversationApi';
+
 import ChatItem from './ChatItem';
+import Error from './ui/Error';
 
 export default function ChatItems() {
+	const { user } = useSelector((state) => state.auth);
+	const {
+		data: conversations,
+		isLoading,
+		isError: processIsError,
+		error: processError,
+	} = useGetConversationsQuery(user?.email);
+
+	// decide what content to render
+	let content;
+
+	if (isLoading) {
+		content = (
+			<p className='text-slate-700 font-semibold m-3'>
+				Loading conversations...
+			</p>
+		);
+	} else if (!isLoading && processIsError) {
+		content = <Error message={processError.data} />;
+	} else if (!isLoading && !processIsError && conversations.length === 0) {
+		content = (
+			<p className='text-slate-700 font-semibold m-3'>
+				No conversations found...
+			</p>
+		);
+	} else if (!isLoading && !processIsError && conversations.length > 0) {
+		content = conversations
+			.toSorted((a, b) => b.timestamp - a.timestamp)
+			.map((conversation) => (
+				<ChatItem key={conversation.id} conversation={conversation} />
+			));
+	}
+
 	return (
 		<ul>
 			<li>
-				<ChatItem
+				{/* <ChatItem
 					avatar='https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg'
 					name='Saad Hasan'
 					lastMessage='bye'
 					lastTime='25 minutes'
 				/>
-				<ChatItem
-					avatar='https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg'
-					name='Sumit Saha'
-					lastMessage='will talk to you later'
-					lastTime='10 minutes'
-				/>
-				<ChatItem
-					avatar='https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg'
-					name='Mehedi Hasan'
-					lastMessage='thanks for your support'
-					lastTime='15 minutes'
-				/>
+*/}
+				{content}
 			</li>
 		</ul>
 	);
