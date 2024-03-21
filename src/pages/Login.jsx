e4495cafd7c3406e6ec6e73c02e1f4a5199131ab
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-03-21 14:13:47 +0600

    Added: created login mutation and implemented login functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,50 @@
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
 import Error from '../components/ui/Error';
 import { lwsLightLogo } from '../assets';
 
+import { useLoginMutation } from '../features/auth/authApi';
+
 export default function Login() {
+	const [data, setData] = useState({
+		email: '',
+		password: '',
+	});
+	const [error, setError] = useState('');
+	const navigate = useNavigate();
+
+	const [login, { isSuccess, isError: processIsError, error: processError }] =
+		useLoginMutation();
+
+	const handleLogin = (e) => {
+		e.preventDefault();
+
+		login(data);
+	};
+
+	useEffect(() => {
+		setError('');
+	}, [data.email, data.password]);
+
+	useEffect(() => {
+		if (isSuccess) {
+			navigate('/inbox');
+		}
+	}, [navigate, isSuccess]);
+
+	useEffect(() => {
+		if (processIsError) {
+			setError(processError.data);
+		}
+	}, [processIsError, processError?.data]);
+
 	return (
 		<div className='grid place-items-center h-screen bg-[#F9FAFB'>
 			<div className='min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
 				<div className='max-w-md w-full space-y-8'>
 					<div>
-						<Link to='/'>
+						<Link to='/inbox'>
 							<img
 								className='mx-auto h-12 w-auto'
 								src={lwsLightLogo}
@@ -19,8 +55,7 @@ export default function Login() {
 							Sign in to your account
 						</h2>
 					</div>
-					<form className='mt-8 space-y-6' action='#' method='POST'>
-						<input type='hidden' name='remember' value='true' />
+					<form className='mt-8 space-y-6' onSubmit={handleLogin}>
 						<div className='rounded-md shadow-sm -space-y-px'>
 							<div>
 								<label
@@ -36,8 +71,16 @@ export default function Login() {
 									required
 									className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm'
 									placeholder='Email address'
+									value={data.email}
+									onChange={(e) =>
+										setData((prev) => ({
+											...prev,
+											email: e.target.value,
+										}))
+									}
 								/>
 							</div>
+
 							<div>
 								<label htmlFor='password' className='sr-only'>
 									Password
@@ -50,6 +93,13 @@ export default function Login() {
 									required
 									className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm'
 									placeholder='Password'
+									value={data.password}
+									onChange={(e) =>
+										setData((prev) => ({
+											...prev,
+											password: e.target.value,
+										}))
+									}
 								/>
 							</div>
 						</div>
@@ -72,7 +122,7 @@ export default function Login() {
 							</button>
 						</div>
 
-						<Error message='There was an error' />
+						{error && <Error message={error} />}
 					</form>
 				</div>
 			</div>
