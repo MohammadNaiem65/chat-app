cbf4e03566721ca9951e58b1e717cb2dc86bec18
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-03-27 03:07:05 +0600

    Updated: set up socket at getMessages endpoint to receive messages

--- src/features/message/messageApi.js
+++ src/features/message/messageApi.js
@@ -1,10 +1,60 @@
+import { io } from 'socket.io-client';
 import apiSlice from '../api/apiSlice';
+import { useFindUser } from '../../hooks';
 
 const messagesApi = apiSlice.injectEndpoints({
 	endpoints: (builder) => ({
 		getMessages: builder.query({
 			query: ({ conversationId }) =>
 				`/messages?conversationId=${conversationId}&_sort=timestamp&_order=desc&_page=1&_limit=5`,
+			async onCacheEntryAdded(
+				args,
+				{
+					cacheDataLoaded,
+					cacheEntryRemoved,
+					updateCachedData,
+					getState,
+				}
+			) {
+				// create socket instance
+				const socket = io('http://localhost:9000', {
+					withCredentials: true,
+				});
+
+				try {
+					await cacheDataLoaded;
+
+					socket.on('message', (data) => {
+						const { data: message } = data || {};
+						const { conversationId, receiver } = message || {};
+
+						const { email: userEmail } = getState().auth.user;
+						// check if the message is for the current user
+						const messageForUser = useFindUser(userEmail, [
+							receiver,
+						]);
+
+						if (messageForUser?.id) {
+							updateCachedData((draft) => {
+								const messageToEdit = draft.find(
+									(message) =>
+										message?.conversationId ===
+										conversationId
+								);
+
+								if (messageToEdit?.conversationId) {
+									draft.push(message);
+								}
+							});
+						}
+					});
+				} catch (error) {
+					// do nothing
+				}
+
+				await cacheEntryRemoved;
+				socket.close();
+			},
 		}),
 		addMessage: builder.mutation({
 			query: ({ data }) => ({
